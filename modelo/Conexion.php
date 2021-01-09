69d7bf660fb3ed4bd57c53c46d72dcdc6dba34b2
Emmanuel-10 <eescalonabustos@gmail.com>
2021-01-08 21:40:48 -0300

    Trabajo en la Nube

--- modelo/Conexion.php
+++ modelo/Conexion.php
@@ -3,9 +3,9 @@ namespace modelo;
 
 class Conexion{
 
-    public static $user="root";
-    public static $pass="";
-    public static $URL="mysql:host=localhost;dbname=optica_2020";
+    public static $user="uj88dolyb2vl1pua";
+    public static $pass="BPi71n4vRdio5y6XUCMY";
+    public static $URL="mysql:host=bsqzvvsytf4jhw3nvzn1-mysql.services.clever-cloud.com;dbname=bsqzvvsytf4jhw3nvzn1";
 
     //public static $user="root";
     //public static $pass="";
